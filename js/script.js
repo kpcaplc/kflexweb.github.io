b15a16eabcf7fc14fc8728d3396085a9186166b4
KPCA Plc. <122080703+kpcaplc@users.noreply.github.com>
2025-06-21 14:37:53 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const btnToggle = document.getElementById("toggle");
+const navbarMobile = document.getElementById("navbar-mobile");
+
+// btnToggle.onclick = function () {
+//   navbarMobile.classList.toggle("active");
+// };
+
+btnToggle.onclick = () => navbarMobile.classList.toggle("active");
+
+// const btnToggle = document.getElementById("btnToggle");
+// const mobileNabar = document.getElementById("mobile-navbar");
+
+// // btnToggle.onclick = function () {
+// //   mobileNabar.classList.toggle("active");
+// // };
+
+// btnToggle.onclick = () => mobileNabar.classList.toggle("active");
